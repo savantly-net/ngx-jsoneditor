956e490f32a3b4ecdbcb6d825f5d92dd8ca58883
Jeremy <jdbranham@hotmail.com>
2018-02-13 11:42:24 -0600

    export component

--- src/app/modules/jsoneditor/jsoneditor.module.ts
+++ src/app/modules/jsoneditor/jsoneditor.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { JsonEditorComponent } from './jsoneditor.component';
+export { JsonEditorComponent } from './jsoneditor.component';
 
 @NgModule({
   imports: [
